04c944b4e976807b63dff7cdba568a7ceeb36a88
trinhngockhang <20168267@student.hust.edu.vn>
2017-06-14 15:45:45 +0700

    Login,Login with FB,Log Out

--- app.js
+++ app.js
@@ -2,30 +2,25 @@ const express= require('express');
 const config= require('./config.json');
 const mongoose= require('mongoose');
 const examsRouter= require(__dirname+ '/modules/api/exams/');
+const usersRouter= require(__dirname+ '/modules/api/users/');
 const bodyParser= require('body-parser');
-const exphbs = require("express-handlebars");
-const path = require('path');
+const Passport = require('passport');
 
-var app = express();
 
-// Setup Handlebars engine
-app.engine('.hbs', exphbs({
-  defaultLayout: 'layout',
-  partialsDir: __dirname + '/views/partials',
-  layoutsDir: __dirname + '/views/layouts',
-  extname: '.hbs'
-}))
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'views'));
+const app= express();
+
+app.set('users','./users');
+app.set('view engine', 'html');
+
 
 app.use(express.static(__dirname+ '/public'));
 app.use(bodyParser.json({extend: true}));
 app.use(bodyParser.urlencoded({extend: true}));
 app.use('/exams', examsRouter);
+app.use('/users', usersRouter);
 
-app.use('/', function (req,res) {
-  res.render('home');
-})
+
+
 mongoose.connect(config.connectionString, (err)=>{
   if(err){
     console.log(err);
---
+++ modules/api/users/usersModel.js
@@ -0,0 +1,43 @@
+  const mongoose = require('mongoose');
+  const bcrypt = require('bcrypt');
+  const Schema = mongoose.Schema;
+
+  var usersSchema = new Schema({
+    id : {type : Number,require : true, unique: true},
+    username : {type : String,require : true,unique:true},
+    password : { type : String,require : true,minlength : 6},
+    email :
+    { type : String, required : true, unique : true, validate : {
+      validator :
+        (value) => {
+          var regex = /@/;
+          return regex.test(value);
+        },
+        message : '{VALUE} is not valid email',
+    } },
+    avatar : { type : String , default : ''},
+    createdDate :  { type : Date, default : new Date().toISOString() },
+    updatedDate :  { type : Date, default : new Date().toISOString() },
+    // profile : { type : ObjectId, ref : 'profiles', default : null},
+    rank: {type : Number,require : true},
+    point: {type :Number}
+  })
+
+  usersSchema.pre('save', function (next) {
+    var user = this;
+    console.log("this is user",user);
+
+    if (!user.isModified('password')) {
+      return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    })
+  });
+
+
+  module.exports = mongoose.model('users', usersSchema);
