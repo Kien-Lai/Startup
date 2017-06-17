eba3110d5f3a1588fc16c1e022a83a64164ea41b
inuyashaaa <huymanhtmhp@gmail.com>
2017-06-17 15:20:22 +0700

    Chinh lai cau truc views va chuyen view engine sang handlebar

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-const express= require('express');
-const config= require('./config.json');
-const mongoose= require('mongoose');
-const examsRouter= require(__dirname+ '/modules/api/exams/');
-const usersRouter= require(__dirname+ '/modules/api/users/');
-const bodyParser= require('body-parser');
+const express = require('express');
+const config = require('./config.json');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const Passport = require('passport');
-
+const hbs = require('express-handlebars');
+const path = require('path');
+const examsRouter = require(__dirname+ '/modules/api/exams/');
+const usersRouter = require(__dirname+ '/modules/api/users/');
 
 const app= express();
 
+app.use(express.static(__dirname + '/public'));
+// Set up handlebars engine
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + 'views/partials/'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.set('users','./users');
-app.set('view engine', 'html');
-
-
-app.use(express.static(__dirname+ '/public'));
 app.use(bodyParser.json({extend: true}));
 app.use(bodyParser.urlencoded({extend: true}));
 app.use('/exams', examsRouter);
 app.use('/users', usersRouter);
 
+app.get('/', (req, res) =>{
+  res.render('home');
+});
 
 
 mongoose.connect(config.connectionString, (err)=>{
   if(err){
     console.log(err);
   }else{
-    console.log('connect DB success !');
+    console.log('Connect DB success !');
   }
 })
 
 app.listen(config.port, (req, res) => {
-  console.log(`app listen on ${config.port}`);
+  console.log(`App listen on ${config.port}`);
 })
